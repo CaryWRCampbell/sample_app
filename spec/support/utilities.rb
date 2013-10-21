319f0e3cc72af2d0029c46614787320064b298b0
Cary WR Campbell <cwrc@xpasc.com>
2013-10-21 16:09:19 -0400

    Include 5.6 Exercises.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# The following is deprecated.  full_title is now implemented in spec/helpers/application_helper_spec.rb.
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
