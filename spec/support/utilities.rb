ef52d422980e1c05b17996fc0c96c9a4aa8f6bb7
Cary WR Campbell <cwrc@xpasc.com>
2013-10-24 14:38:58 -0400

    Finish sign in.

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,17 @@
 include ApplicationHelper
 
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
 # The following is deprecated.  full_title is now implemented in spec/helpers/application_helper_spec.rb.
 #def full_title(page_title)
 #  base_title = "Ruby on Rails Tutorial Sample App"
